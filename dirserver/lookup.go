28daf7c79a833225d85c9f0fcc9a6f38c2e0fb07
Victor van Poppelen <vvanpo@gmail.com>
2025-01-20 22:26:21 +0100

    dir: describe Lookup() requirements

--- dirserver/lookup.go
+++ dirserver/lookup.go
@@ -6,6 +6,16 @@ import (
 	"upspin.io/upspin"
 )
 
+/*
+If the Lookup path...
+- contains any link elements (including the final element), return the link closest to the root and upspin.ErrFollowLink if the user has any access right on the link, else errors.Private
+- does not grant any access rights to the requester, return errors.Private
+- does not exist, return errors.NotExist
+- exists and...
+  - grants the requester read rights or is a special access or group file, return the entry
+  - grants any right (but not access.Read), return the entry without blocks or packing data and marked as incomplete
+*/
+
 // Lookup implements upspin.DirServer.
 func (d *dialed) Lookup(name upspin.PathName) (*upspin.DirEntry, error) {
 	ctx, op := d.setCtx("Lookup", name)
--- dirserver/whichaccess.go
+++ dirserver/whichaccess.go
@@ -16,11 +16,11 @@ If the WhichAccess path...
 - look for the nearest directory entry on the path (including the final element) that contains an entry named Access, regardless of whether the path or some of its ancestors exist, and..
   - if no Access file is found and the user is the owner of the tree, return nil, else errors.Private
   - if an Access file is found, and the user has any access right described within, return the entry for the Access file, else errors.Private
-	- if the user has no read rights, return the entry without blocks or packdata
+    - if the user has no read rights, return the entry without blocks or packdata and marked as incomplete
 
 TODO snapshot trees; always return nil
 TODO if a request with a non-existent path contains an ancestor element that is a file instead of a directory, what should be returned?
-	- the reference implementation just pretends it's a folder; e.g. WhichAccess("user@example.com/Access/Access") (which is never a possible path) will return "user@example.com/Access" if it exists.
+  - the reference implementation just pretends it's a folder; e.g. WhichAccess("user@example.com/Access/Access") (which is never a possible path) will return "user@example.com/Access" if it exists.
 */
 
 // WhichAccess implements upspin.DirServer.
